38be8dba8148ef9039fa9e448ce899645aa26b5c
xanax <34755589+anKordii@users.noreply.github.com>
2023-05-02 18:58:20 +0200

    tts

---
+++ pages/tts.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect } from "react";
+import { socket } from "@components/socket";
+import toast, { Toaster } from "react-hot-toast";
+
+export default function Home({ channel }) {
+  useEffect(() => {
+    if (!channel) {
+      toast.error("Zapomniałeś/aś o ?channel= w URL strony.", {
+        duration: 6000,
+      });
+
+      setInterval(() => {
+        toast.error("Zapomniałeś/aś o ?channel= w URL strony.", {
+          duration: 6000,
+        });
+      }, 3000);
+    }
+  }, [channel]);
+
+  useEffect(() => {
+    function onConnect() {
+      toast.success("Połączono z serwerem - TTVUpdates");
+    }
+
+    function onDisconnect() {
+      toast.error("Rozłączono z serwerem - TTVUpdates");
+    }
+
+    function onFooEvent(value) {
+      callNotify(value);
+    }
+
+    function onRefreshEvent() {
+      window.location.reload();
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("new-tts", onFooEvent);
+    socket.on("refresh-overlay", onRefreshEvent);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("new-tts", onFooEvent);
+      socket.off("refresh-overlay", onRefreshEvent);
+    };
+  }, []);
+
+  async function callNotify(value) {
+    
+    if (channel !== value.channel) {
+      return;
+    }
+
+    const audioPlayer = new Audio(`https://ai-cdn.oxynstudios.com/ai.mp3?v=${new Date().getTime()}`);
+    audioPlayer.load()
+    await audioPlayer.play()
+  }
+
+  return (
+    <>
+      <Toaster
+        position="top-left"
+        containerStyle={{
+          top: "50%",
+        }}
+      />
+    </>
+  );
+}
+export async function getServerSideProps({ query }) {
+  const { channel } = query;
+  return {
+    props: {
+      channel: channel || null,
+    },
+  };
+}
